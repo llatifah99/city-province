650092941880ea76a82243ca67bee9d9a6eef793
llatifah99 <litalatifah6@gmail.com>
2024-07-12 10:24:45 +0700

    edit-2

--- index.js
+++ index.js
@@ -23,6 +23,21 @@ app.get("/province", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+app.get("/city", async (req, res) => {
+  try {
+    const apiKey = process.env.API_KEY;
+    const url = "https://api.rajaongkir.com/starter/city";
+    const response = await axios.get(url, {
+      headers: {
+        key: apiKey,
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
